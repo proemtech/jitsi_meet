3a2bc52978217f3a5480dcf2d6e5f6212c1dac73
ProEmTech GIT Admin <info@proemtech.com>
2022-09-23 08:45:24 +0200

    Custom Jitsi Meet

---
+++ lib/jitsi_meet_listener.dart
@@ -0,0 +1,20 @@
+/// Class holding the callback functions for conference events
+class JitsiMeetListener {
+  final Function({Map<dynamic, dynamic>? message})? onConferenceWillJoin;
+  final Function({Map<dynamic, dynamic>? message})? onConferenceJoined;
+  final Function({Map<dynamic, dynamic>? message})? onConferenceTerminated;
+  final Function({Map<dynamic, dynamic>? message})? onPictureInPictureWillEnter;
+  final Function({Map<dynamic, dynamic>? message})?
+      onPictureInPictureTerminated;
+  final Function({Map<dynamic, dynamic>? message})? onWhiteboardClicked;
+  final Function(dynamic error)? onError;
+
+  JitsiMeetListener(
+      {this.onConferenceWillJoin,
+      this.onConferenceJoined,
+      this.onConferenceTerminated,
+      this.onPictureInPictureWillEnter,
+      this.onPictureInPictureTerminated,
+      this.onWhiteboardClicked,
+      this.onError});
+}
